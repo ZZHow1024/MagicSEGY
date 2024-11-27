d4878e2915066795828bd84f69793587659e38c3
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-11-27 13:05:42 +0800

    refactor: restructure and add new UI components

---
+++ src/MainClass.java
@@ -0,0 +1,11 @@
+import ui.Application;
+
+/**
+ * @author ZZHow
+ * @date 2024/11/25
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        Application.show(args);
+    }
+}
